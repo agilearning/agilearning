384ebf91d425f660e25cb7dd54096a63dae6ebec
c3h3 <c3h3.tw@gmail.com>
2015-01-11 15:37:13 +0800

    init project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='agilearning',
+    version='0.0.1dev',
+    description='agilearning',
+    author='Chia-Chi Chang',
+    author_email='c3h3.tw@gmail.com',
+    packages=find_packages(),
+    #package_data={'': ['*.coffee']},
+    install_requires=[],
+)
